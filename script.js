0b5788d3fc98057a311ac2f4e5d6915ad5eff0a9
Marcin Żebrowski <77941248+marcinzebrowski093@users.noreply.github.com>
2021-01-26 13:53:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+
+document.addEventListener('scroll', ()=>{
+var scroll_position = window.scrollY;
+if(scroll_position > 250){
+    header.style.backgroundColor = "#2d3142";
+}else{
+    header.style.backgroundColor = "transparent";
+}
+});
